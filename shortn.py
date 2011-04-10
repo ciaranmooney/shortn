e065d3007668e9db61548e2eb11a841c9b49a1cd
Ciarán Mooney <general.mooney@googlemail.com>
2011-04-10 16:41:50 +0100

    ,

--- shortn.py
+++ shortn.py
@@ -1,17 +1,35 @@
 #! /usr/bin/env python
 
 import web
+from web import form
 
 render = web.template.render("templates/")
-urls = ( "/(.*)", "index")
+urls = ( "/", "index")
 
 app = web.application(urls, globals())
 
+database = {}
+
+myform = form.Form(
+    form.Textbox("URL", form.notnull),
+    form.Checkbox("Temporary?")
+    )
+
 class index:
-    def GET(self, name):
-        return render.index(name)
+    def GET(self):
+        form = myform()
+        return render.formtest(form)
+
+    def POST(self):
+        form = myform()
+        if not form.validates():
+            return render.formtest(form)
+
+        else:
+          return "Greeet"
 
 if __name__ == "__main__":
+    web.internalerror = web.debugerror
     app.run()
 
 
