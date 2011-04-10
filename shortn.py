d22b13dadfc780f6808ffe853773e21e72c3c12d
Ciarán Mooney <general.mooney@googlemail.com>
2011-04-10 17:19:19 +0100

    Kinda got something that may look like a url shortener.

--- shortn.py
+++ shortn.py
@@ -6,7 +6,8 @@ from web import form
 import hashlib
 
 render = web.template.render("templates/")
-urls = ( "/", "index")
+
+urls = ( "/(.*)", "index")
 
 app = web.application(urls, globals())
 
@@ -18,19 +19,25 @@ myform = form.Form(
     )
 
 class index:
-    def GET(self):
-        form = myform()
-        return render.formtest(form)
+    def GET(self, name):
+        print name
+        
+        if name in database.keys(): 
+            return web.seeother(database[name])
+        else:
+            form = myform()
+            return render.formtest(form)
 
-    def POST(self):
+    def POST(self, unknown):
+        print unknown
         form = myform()
         if not form.validates():
             return render.formtest(form)
 
         else:
           
-          database[hashlib.sha224(form.d.URL).hexdigest()] = form.d.URL
-          return "Greeet", database
+          database[hashlib.sha224(form.d.URL).hexdigest()[:8]] = form.d.URL
+          return database
 
 if __name__ == "__main__":
     web.internalerror = web.debugerror
